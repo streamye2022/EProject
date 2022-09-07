fa2ef93f628b78ae43fc6b02f6f7b8368db15d7a
REDMOND\xiaoxiye <xiaoxiye@microsoft.com>
2022-09-07 12:04:17 +0800

    json enum test

---
+++ Microsoft.Streamye.UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Microsoft.Streamye.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyClass myClass = new MyClass();
+            myClass.SetMyEnum(CheckErrorType.DnsCheckFqdnNotFound);
+
+            Console.WriteLine(JsonConvert.SerializeObject(myClass));
+        }
+        
+        [JsonConverter(typeof(StringEnumConverter))]
+        // [DataContract(Name = "CheckErrorType")]
+        public enum CheckErrorType
+        {
+            [EnumMember(Value = "dnsCheckFqdnNotFound")]
+            DnsCheckFqdnNotFound = 100,
+            
+            [EnumMember(Value = "dnsCheckNameWithInvalidCharacter")]
+            DnsCheckNameWithInvalidCharacter = 101,
+
+        }
+
+        public class MyClass
+        {
+            [JsonProperty("errorType")]
+            private CheckErrorType _myEnum;
+
+            public void SetMyEnum(CheckErrorType myEnum)
+            {
+                _myEnum = myEnum;
+            }
+        }
+
+    }
+}
