bbb0ad45c4577be93a9ae3e96383f03f4f030104
REDMOND\xiaoxiye <xiaoxiye@microsoft.com>
2022-09-11 11:45:04 +0800

    load balance

---
+++ Microsoft.Streamye.Cores/LoadBalance/Extensons/LoadBalanceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Streamye.Cores.LoadBalance.Options;
+
+namespace Microsoft.Streamye.Cores.LoadBalance.Extensons
+{
+    public static class LoadBalanceExtension
+    {
+        public static IServiceCollection AddRandomLoadBalance(this IServiceCollection services)
+        {
+            AddRandomLoadBalance(services, options => { });
+            return services;
+        }
+        
+        //add other load balance
+        public static IServiceCollection AddRandomLoadBalance(this IServiceCollection serviceCollection, Action<LoadBalanceOptions> options)
+        {
+            // LoadBalanceOptions balanceOptions = new LoadBalanceOptions();
+            // options(balanceOptions);
+            serviceCollection.Configure(options);
+
+            //
+            serviceCollection.AddSingleton<ILoadBalance,RandomLoadBalance>();
+            return serviceCollection;
+        }
+    }
+}
---
+++ Microsoft.Streamye.Cores/LoadBalance/Options/LoadBalanceOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Streamye.Cores.LoadBalance.Options
+{
+    public class LoadBalanceOptions
+    {
+        public LoadBalanceOptions()
+        {
+            this.Type = "Random";
+        }
+
+        public string Type { set; get; }
+    }
+}
