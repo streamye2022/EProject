e701f0011b58e9a160b9fbe3b13ed9de65434976
REDMOND\xiaoxiye <xiaoxiye@microsoft.com>
2022-09-16 10:09:48 +0800

    product service

--- Microsoft.Streamye.ProductServices/Controllers/ProductController.cs
+++ Microsoft.Streamye.ProductServices/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCore.CAP;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Streamye.Commons.Exceptions;
 using Microsoft.Streamye.ProductServices.Dtos.Inputs;
 using Microsoft.Streamye.ProductServices.Models;
 using Microsoft.Streamye.ProductServices.Services;
@@ -90,6 +92,51 @@ namespace Microsoft.Streamye.ProductServices.Controllers
             return Ok("更新库存成功");
         }
         
+        // 异步更新秒杀库存
+        [NonAction]
+        [CapSubscribe("product.stock")]
+        public async Task<IActionResult> SetProductStock(StockSetDto productVo)
+        {
+            // 1、查询商品
+            Product product = await productService.GetProductByIdAsync(productVo.ProductId);
+
+            // 2、判断商品库存是否完成
+            if (product.ProductStock <= 0)
+            {
+                throw new BizException("库存完了");
+            }
+
+            // 3、扣减商品库存
+            product.ProductStock = product.ProductStock - productVo.ProductCount;
+
+            // 4、更新商品库存
+            await productService.UpdateAsync(product);
+            return Ok("更新库存成功");
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            await productService.CreateAsync(product);
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await productService.DeleteAsync(product);
+            return product;
+        }
         
         private async Task<bool> ProductExists(int id)
         {
--- Microsoft.Streamye.ProductServices/Controllers/ProductImageController.cs
+++ Microsoft.Streamye.ProductServices/Controllers/ProductImageController.cs
@@ -1,10 +1,116 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Streamye.ProductServices.Models;
+using Microsoft.Streamye.ProductServices.Services;
 
 namespace Microsoft.Streamye.ProductServices.Controllers
 {
     [Route("Products/{productId}/ProductImages")]
     public class ProductImageController : ControllerBase
     {
-        
+        private readonly IProductImageService productImageService;
+
+        public ProductImageController(IProductImageService productImageService)
+        {
+            this.productImageService = productImageService;
+        }
+
+        // GET: api/ProductImages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductImage>>> GetProductImages(int ProductId)
+        {
+            ProductImage productImage = new ProductImage();
+            productImage.ProductId = ProductId;
+            var results = await productImageService.GetProductImagesAsync(productImage);
+            return results.ToList();
+        }
+
+        // GET: api/ProductImages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductImage>> GetProductImage(int id)
+        {
+            var productImage = await productImageService.GetProductImageByIdAsync(id);
+
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+
+            return productImage;
+        }
+
+        // PUT: api/ProductImages/5
+        // PUT 是完整的修改，patch是修改部分字段
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductImage(int id, ProductImage productImage)
+        {
+            if (id != productImage.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await productImageService.UpdateAsync(productImage);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ProductImageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductImages
+        [HttpPost]
+        public async Task<ActionResult<ProductImage>> PostProductImage(ProductImage productImage)
+        {
+            try
+            {
+                productImageService.CreateAsync(productImage);
+            }
+            catch (DbUpdateException)
+            {
+                if (await ProductImageExists(productImage.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProductImage", new { id = productImage.Id }, productImage);
+        }
+
+        // DELETE: api/ProductImages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductImage>> DeleteProductImage(int id)
+        {
+            var productImage = await productImageService.GetProductImageByIdAsync(id);
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+
+            await productImageService.DeleteAsync(productImage);
+            return productImage;
+        }
+
+        private async Task<bool> ProductImageExists(int id)
+        {
+            return await productImageService.ProductImageExistsAsync(id);
+        }
     }
 }
